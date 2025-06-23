fecf1aaa6dc4d376d15a880dedbfa19930016196
Your Name <your.email@example.com>
2025-06-23 07:57:23 +0530

    Add latest frontend URL to CORS (force deployment)

--- server/index.js
+++ server/index.js
@@ -36,6 +36,7 @@ app.use(function(req, res, next) {
     'https://client-q5s7dtxpi-akhashcs24s-projects.vercel.app',
     'https://client-dkydevmtr-akhashcs24s-projects.vercel.app',
     'https://client-gewfgz93u-akhashcs24s-projects.vercel.app',
+    'https://client-awsd7qusd-akhashcs24s-projects.vercel.app',
     'http://localhost:3000',
     'http://localhost:3001'
   ];
